b7a3f4f9268013024e76fd781362d87a676e13ae
Soakeson <skyler.oakeson@gmail.com>
2022-12-07 21:22:49 -0700

    Got the all combos working but tbh I have no clue how this is working. But it does.

--- Teacup.java
+++ Teacup.java
@@ -9,18 +9,29 @@ public class Teacup {
         }
     }
 
+    public void allCombos() {
+        allCombos(10, "", 1);
+    }
+
+    private void allCombos(int amt, String soFar, int currentSize) {
+        StringBuilder sb = new StringBuilder(soFar);
+        if (amt != 10) {sb.append(currentSize);}
+        if (amt == 0) {
+            System.out.println(sb.toString());
+            return;
+        }
+        for (int i=currentSize; i <= amt; i++) {
+            allCombos(amt - i, sb.toString(), i);
+        }
+    }
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
         for (int i=0; i < table.length; i++) {
             sb.append(i);
-            for (int k=0; k < 3 - i / 10; k++) {
-                sb.append(" ");
-            }
-
-            for (int j=0; j < table[i].length; j++) {
-                sb.append(table[i][j] + " ");
-            }
+            for (int k=0; k < 3 - i / 10; k++) { sb.append(" "); }
+            for (int j=0; j < table[i].length; j++) { sb.append(table[i][j] + " ");}
             sb.append("\n");
         }
         return sb.toString();
@@ -28,6 +39,6 @@ public class Teacup {
 
     public static void main(String[] args) {
         Teacup tc = new Teacup();
-        System.out.println(tc);
+        tc.allCombos();
     }
 }

4d693c82d0b15d27086670891610dd77f1c681c8
Soakeson <skyler.oakeson@gmail.com>
2022-12-11 13:38:25 -0700

    Finished program time to turn it in.

--- Teacup.java
+++ Teacup.java
@@ -1,12 +1,28 @@
+import java.util.Hashtable;
+
 public class Teacup {
     private int[][] table = new int[12][24];
+    private Hashtable<Integer, Integer> prices = new Hashtable<>();
 
     public Teacup() {
         for (int i=0; i < table.length; i++) {
-            for (int j=0; j < table[i].length; j++) {
+            for (int j=0; j < table[1].length; j++) {
                 table[i][j] = 0;
             }
         }
+
+        prices.put(1,1);
+        prices.put(2,3);
+        prices.put(3,5);
+        prices.put(4,9);
+        prices.put(5, 10);
+        prices.put(6, 15);
+        prices.put(7, 17);
+        prices.put(8, 18);
+        prices.put(9, 19);
+        prices.put(10, 22);
+        prices.put(11, 25);
+        prices.put(12, 27);
     }
 
     public void allCombos() {
@@ -25,13 +41,63 @@ public class Teacup {
         }
     }
 
+    /**
+     * ITERATIVE SOLUTION
+     */
+    public void buildTable() {
+        for (int i=0; i < table.length; i++) {
+            for (int j=0; j < table[1].length; j++) {
+                int setSize = i+1;
+                int numCups = j+1;
+                int priceSetIncl = 0;
+
+                if (numCups >= setSize){
+                    numCups -= setSize;
+                    priceSetIncl += prices.get(setSize);
+                }
+                if (numCups > 0) {
+                    priceSetIncl += table[i][numCups-1];
+                }
+                if (i > 0 && priceSetIncl <= table[i-1][j]) {
+                    table[i][j] = table[i-1][j];
+                } else {
+                    table[i][j] = priceSetIncl;
+                }
+            }
+        }
+    }
+
+    public String bestSum(int numCups) {
+        StringBuilder sb = new StringBuilder();
+        numCups -= 1;
+        int setSize = 11;
+        sb.append(table[setSize][numCups] + "$ ");
+        while(numCups > 0) {
+            if (table[setSize][numCups] > table[setSize-1][numCups]) {
+                numCups -= setSize+1;
+                sb.append(setSize+1 + " ");
+            } else {
+                setSize -= 1;
+            }
+        }
+        return sb.toString();
+    }
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
-
-        for (int i=0; i < table.length; i++) {
+        sb.append("   ");
+        for (int i=1; i <= table[1].length; i++) {
             sb.append(i);
-            for (int k=0; k < 3 - i / 10; k++) { sb.append(" "); }
-            for (int j=0; j < table[i].length; j++) { sb.append(table[i][j] + " ");}
+            for (int l=0; l < 3 - String.valueOf(i).length(); l++) { sb.append(" "); }
+        }
+        sb.append("\n");
+        for (int i=1; i <= table.length; i++) {
+            sb.append(i);
+            for (int l=0; l < 3 - String.valueOf(i).length(); l++) { sb.append(" "); }
+            for (int j=0; j < table[1].length; j++) { 
+                sb.append(table[i-1][j]);
+                for (int l=0; l < 3 - String.valueOf(table[i-1][j]).length(); l++) { sb.append(" "); }
+            }
             sb.append("\n");
         }
         return sb.toString();
@@ -39,6 +105,10 @@ public class Teacup {
 
     public static void main(String[] args) {
         Teacup tc = new Teacup();
-        tc.allCombos();
+        tc.buildTable();
+        System.out.println(tc);
+        for (int i = 1; i <= 24; i++) {
+            System.out.println(tc.bestSum(i));
+        }
     }
 }
